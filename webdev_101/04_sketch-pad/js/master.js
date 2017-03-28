f7ca7536dda62aeae964d1037408a4a3d414280a
Etienne Boisseau <etienne.boisseau@ensam.eu>
2017-03-28 15:16:50 -0400

    add settings sidebar

--- webdev_101/04_sketch-pad/js/master.js
+++ webdev_101/04_sketch-pad/js/master.js
@@ -23,3 +23,57 @@ $(document).ready(
         });
     }
 );
+$(document).ready(
+    function(){
+        $("#settings-size").on('click',function(){
+            $("#subsettings-size").slideToggle(250);
+        });
+    }
+);
+$(document).ready(
+    function(){
+        $("#settings-color").on('click',function(){
+            $("#subsettings-color").slideToggle(250);
+        });
+    }
+);
+$(document).ready(
+    function(){
+        $("#settings-button").on('click',function(){
+            $("#settings-mainlist").slideToggle(250);
+        });
+    }
+);
+
+var drawColor = '#555'
+
+var pickColor = function(colorName){
+    switch (colorName) {
+        case 'pink':
+            drawColor = '#f0a4ab'
+            break;
+        case 'green':
+            drawColor = '#d9e0b0'
+            break;
+        case 'maroon':
+            drawColor = '#f9dc86'
+            break;
+        case 'orange':
+            drawColor = '#ffa517'
+            break;
+        case 'red':
+            drawColor = '#f57650'
+            break;
+        case 'blue':
+            drawColor = '#3591d0'
+            break;
+        case 'lightblue':
+            drawColor = '#bdcee2'
+            break;
+        case 'black':
+            drawColor = '#555'
+            break;
+        default:
+            drawColor = '#555554'
+    }
+}
